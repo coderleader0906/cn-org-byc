5a7410204d71945eadd99d6c0585e7aac82b5633
Ken.Zhang <CoderLeader0906@gmail.com>
2024-02-10 17:03:02 +0800

    style(N/A): 格式化代码

--- src/cn-org-byc-base-starter/src/main/java/cn/org/byc/base/support/beans/UniversalBeanRegistration.java
+++ src/cn-org-byc-base-starter/src/main/java/cn/org/byc/base/support/beans/UniversalBeanRegistration.java
@@ -32,14 +32,17 @@ import org.springframework.core.convert.converter.GenericConverter;
 import java.util.*;
 
 /**
- * UniversalBeanRegistration类实现了BeanPostProcessor和ApplicationContextAware接口，
- * 用于在Spring容器的bean生命周期中执行自定义的后处理任务，以及访问和操作应用上下文。
+ * 实现{@link BeanPostProcessor}和{@link ApplicationContextAware}接口的类，
+ * 用于在Spring容器中对bean实例进行自定义的后处理操作，并能够访问和操作应用上下文。
+ * 此类负责在bean初始化后注册自定义的属性编辑器、转换器、序列化器和反序列化器。
  *
  * @author ken
  */
 public class UniversalBeanRegistration implements BeanPostProcessor, ApplicationContextAware {
 
-    // 使用单例模式，确保整个应用中只有一个UniversalBeanRegistration实例。
+    /**
+     * 类的单例实例
+     */
     public final static UniversalBeanRegistration INSTANCE = new UniversalBeanRegistration();
 
     // 标记是否已初始化，防止重复初始化。
@@ -47,7 +50,7 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
 
     // 存储自定义的bean属性编辑器、转换器和序列化/反序列化器的列表。
     private List<BeansPropertyEditor> editorList = Collections.emptyList();
-    private List<Converter<?,?>> converterList = Collections.emptyList();
+    private List<Converter<?, ?>> converterList = Collections.emptyList();
     private List<GenericConverter> genericConverterList = Collections.emptyList();
     private List<JsonDeserializer<?>> jsonDeserializerList = Collections.emptyList();
     private List<JsonSerializer<?>> jsonSerializerList = Collections.emptyList();
@@ -55,10 +58,13 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
 
     /**
      * 在bean初始化之后调用，用于注册自定义编辑器、转换器、序列化器和反序列化器。
-     * @param bean the new bean instance
-     * @param beanName the name of the bean
-     * @return
-     * @throws BeansException
+     *
+     * @param bean     当前正在创建的bean实例。
+     * @param beanName 当前bean的名称
+     *
+     * @return 返回可能经过处理的bean实例，也可能是原始的bean实例。
+     *
+     * @throws BeansException 如果在处理过程中发生错误。
      */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
@@ -101,9 +107,11 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
     }
 
     /**
-     * 当ApplicationContext可用时调用，用于执行初次注册。
-     * @param applicationContext the ApplicationContext object to be used by this object
-     * @throws BeansException
+     * 设置应用上下文，并在设置后进行自定义编辑器、转换器、序列化器和反序列化器的初始化注册
+     *
+     * @param applicationContext Spring应用上下文
+     *
+     * @throws BeansException 如果在设置应用上下文时发生错误
      */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -111,8 +119,10 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
     }
 
     /**
-     * 执行初次注册，从应用上下文或通过ServiceLoader加载自定义编辑器、转换器、序列化器和反序列化器。
-     * @param applicationContext
+     * 执行自定义编辑器、转换器、序列化器和反序列化器的初始化注册。
+     * 该方法从应用上下文或通过ServiceLoader加载这些组件。
+     *
+     * @param applicationContext 可以为null，如果非null，则从中获取组件实例
      */
     public void initialRegistration(ApplicationContext applicationContext) {
         // 从应用上下文获取或通过ServiceLoader加载自定义编辑器，并添加到editorList。
@@ -122,9 +132,9 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
 
         // 重复上述过程，加载并添加自定义转换器到editorList。
         Iterator<BeansPropertyEditor> beansPropertyEditorIterator = ServiceLoader.load(BeansPropertyEditor.class).iterator();
-        for (;beansPropertyEditorIterator.hasNext();){
+        for (; beansPropertyEditorIterator.hasNext(); ) {
             BeansPropertyEditor next = beansPropertyEditorIterator.next();
-            if (!editorList.contains(next)){
+            if (!editorList.contains(next)) {
                 editorList.add(next);
             }
         }
@@ -136,9 +146,9 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
             converterList.add(converter);
         }
         Iterator<Converter> converterIterator = ServiceLoader.load(Converter.class).iterator();
-        for (;converterIterator.hasNext();){
+        for (; converterIterator.hasNext(); ) {
             Converter next = converterIterator.next();
-            if (!converterList.contains(next)){
+            if (!converterList.contains(next)) {
                 converterList.add(next);
             }
         }
@@ -151,9 +161,9 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
         }
 
         Iterator<GenericConverter> genericConverterIterator = ServiceLoader.load(GenericConverter.class).iterator();
-        for(;genericConverterIterator.hasNext();){
+        for (; genericConverterIterator.hasNext(); ) {
             GenericConverter next = genericConverterIterator.next();
-            if (!genericConverterList.contains(next)){
+            if (!genericConverterList.contains(next)) {
                 genericConverterList.add(next);
             }
         }
@@ -165,9 +175,9 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
             jsonSerializerList.add(jsonSerializer);
         }
         Iterator<JsonSerializer> jsonSerializerIterator = ServiceLoader.load(JsonSerializer.class).iterator();
-        for (;jsonSerializerIterator.hasNext();){
+        for (; jsonSerializerIterator.hasNext(); ) {
             JsonSerializer next = jsonSerializerIterator.next();
-            if (!jsonSerializerList.contains(next)){
+            if (!jsonSerializerList.contains(next)) {
                 jsonSerializerList.add(next);
             }
         }
@@ -180,14 +190,24 @@ public class UniversalBeanRegistration implements BeanPostProcessor, Application
         }
 
         Iterator<JsonDeserializer> jsonDeserializerIterator = ServiceLoader.load(JsonDeserializer.class).iterator();
-        for (;jsonSerializerIterator.hasNext();){
+        for (; jsonSerializerIterator.hasNext(); ) {
             JsonDeserializer<?> next = jsonDeserializerIterator.next();
-            if (!jsonDeserializerList.contains(next)){
+            if (!jsonDeserializerList.contains(next)) {
                 jsonDeserializerList.add(next);
             }
         }
     }
 
+    /**
+     * 从应用上下文中获取指定类型的bean集合。
+     * 如果应用上下文为null，则返回一个空集合。
+     *
+     * @param clazz              要获取的bean的类型。
+     * @param applicationContext Spring应用上下文。
+     * @param <T>                bean的类型。
+     *
+     * @return 返回找到的bean实例的集合，或空集合。
+     */
     private <T> Collection<T> getFromContext(Class<T> clazz, ApplicationContext applicationContext) {
         return applicationContext != null ? applicationContext.getBeansOfType(clazz).values() : Collections.emptyList();
     }
